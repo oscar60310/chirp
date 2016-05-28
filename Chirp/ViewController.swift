b3d72b2930bc6f67c5141f66ee7915f8086f2f0a
Chen Ping Tsai <oscar60310@gmail.com>
2016-05-28 17:27:35 +0800

    xcode inti files

---
+++ Chirp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chirp
+//
+//  Created by Chen Ping Tsai on 2016/5/28.
+//  Copyright © 2016年 NsysuDopp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
