ef9ff2837dc3d032ff8f2185049e7d28731f2ad9
Tsai chen ping <oscar60310@gmail.com>
2016-06-04 20:12:02 +0800

    api 定義

--- chirp-api/register.py
+++ chirp-api/register.py
@@ -20,6 +20,18 @@ class MainPage(webapp2.RequestHandler):
         else:
           out = db.GqlQuery('SELECT * FROM Member WHERE SchoolMail = :1',email)
           if out.get() == None:
+            url = 'https://graph.facebook.com/v2.6/debug_token?input_token=' + token + '&access_token=1608783402765050|a4bf3905aa24c68a90d33575e577e5af'
+            response = urlfetch.fetch(url)
+            data = json.loads(response.content)
+            comfirm = False
+            if 'data' in data:
+              if 'app_id' in data['data']:
+                if data['data']['app_id'] == '1608783402765050':
+                  comfirm = True
+            if not comfirm:
+              self.response.write(json.dumps({"Statu" : "407","Description" : "Token comfirm fail."}))
+             # print(data)
+              return
 
             url = 'https://graph.facebook.com/v2.6/me?fields=id%2Cname%2Cemail%2Clast_name%2Cfirst_name&access_token='+token 
             response = urlfetch.fetch(url)
