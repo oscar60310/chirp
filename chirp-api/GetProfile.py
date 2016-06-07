a096f700c25cd3d9c9f937c035f01bf6b4be1402
Tsai chen ping <oscar60310@gmail.com>
2016-06-08 02:58:47 +0800

    打倒鬼了!!!!!!

--- chirp-api/GetProfile.py
+++ chirp-api/GetProfile.py
@@ -9,7 +9,9 @@ class Member(db.Model):
   member_id  = db.StringProperty()
   name = db.StringProperty()
   first_name = db.StringProperty()
-
+class Token(db.Model):
+  token  = db.StringProperty()
+  member_id = db.StringProperty() 
 class MainPage(webapp2.RequestHandler):
     def get(self):
       import sys
@@ -17,11 +19,12 @@ class MainPage(webapp2.RequestHandler):
       sys.setdefaultencoding('utf8')
       token = self.request.get("token")
      
-      out = db.GqlQuery('SELECT * FROM Member WHERE chirp_token = :1',token).get()
+      out = db.GqlQuery('SELECT * FROM Token WHERE token = :1',token).get()
       if out == None:
         self.response.write(json.dumps({"Statu" : "403","Description" : "Token not vail."}))
         return
-      self.response.write(json.dumps({"Statu" : "200","data" : {"id" : out.member_id , "name" : out.name , "first_name" : out.first_name}}))
+      out2 = db.GqlQuery('SELECT * FROM Member WHERE member_id = :1',out.member_id).get()
+      self.response.write(json.dumps({"Statu" : "200","data" : {"id" : out2.member_id , "name" : out2.name , "first_name" : out2.first_name}}))
        # self.response.write(json.dumps({"Statu" : "405","Description" : "Token not vail"}))
            
 app = webapp2.WSGIApplication([
